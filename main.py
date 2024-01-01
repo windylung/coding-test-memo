26023fad1bc651390a85efbdfbbf054a3e07c0e3
windylung <jisoolee031@gmail.com>
2024-01-01 13:45:13 +0000

    fix.time_complexity_#11653

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ N = int(input())
 def divider(N) :
   if N == 1 :
     return
-
-  for i in range(2, N+1) :
-    while N % i == 0 :
-      N = N // i
-      print(i)
+  elif N % 2 :
+    for i in range(1, int((N-1)/2) + 1) :
+      j = 2*i + 1
+      while N % j == 0 :
+        N = N // j
+        print (j)
+      
+  else :
+    while N % 2 == 0 :
+      N = N // 2
+      print(2)
+    divider(N)
 divider(N)
