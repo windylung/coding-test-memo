afba7e26c8f510f555fb94a212037b10c66b02af
windylung <jisoolee031@gmail.com>
2024-01-04 01:04:52 +0000

    #1789

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print(sorted(map(int, input().split()))[1])
+#1789
+def sum(S) :
+  num = 0
+  N = 1
+  while num < S :
+    num += N
+    N += 1
+  if num == S :
+    print(N-1)
+  else :
+    print(N-2)
+    
+S = int(input())
+sum(S)
