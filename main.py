67516c6a3cf8254939998d7959e3f248f7a9bb5b
windylung <jisoolee031@gmail.com>
2024-01-04 00:25:41 +0000

    #10817

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-N = int(input())
-def divider(N) :
-  if N == 1 :
-    return
-  elif N % 2 :
-    for i in range(1, int((N-1)/2) + 1) :
-      j = 2*i + 1
-      while N % j == 0 :
-        N = N // j
-        print (j)
-      
-  else :
-    while N % 2 == 0 :
-      N = N // 2
-      print(2)
-    divider(N)
-divider(N)
+print(sorted(map(int, input().split()))[1])
