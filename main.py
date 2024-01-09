c98e4c84d353c8734b7df926dce069cca05916c6
windylung <jisoolee031@gmail.com>
2024-01-09 14:50:21 +0000

    #14888

--- main.py
+++ main.py
@@ -1,32 +1,66 @@
-#14889
+#14888
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
-def get_sum(arr) :
-  sum = 0
-  for i in arr :
-    for j in arr :
-      sum += S[i][j]
-  return sum 
-
-
-def get_val(arr, val):
-  if len(arr) == N / 2:
-    other = list(set(range(N)) - set(arr))
-    
-    return (abs(get_sum(other) - val))
-  res = []
-  for i in range(arr[-1] + 1, int(N/2) + len(arr) + 1) :
-    val2 = val
-    for j in arr :
-      val2 = val2 + S[i][j] + S[j][i]
-    arr2 = arr + [i]
-    res.append(get_val(arr2, val2))
-  return min(res)
-
-
 N = int(input())
-S = [list(map(int, input().split())) for _ in range(N)]
-arr = [0]
-print(get_val(arr, 0))
+num = list(map(int, input().split()))
+op = list(map(int, input().split()))
+
+
+def get_idx(op):
+  idx = []
+  for i in range(4):
+    if op[i]:
+      idx.append(i)
+  return idx
+
+
+def cal(n1, n2, op):
+  if op == 0:
+    return n1 + n2
+  elif op == 1:
+    return n1 - n2
+  elif op == 2:
+    return n1 * n2
+  elif op == 3 and n1 < 0 and n2 > 0:
+    return (-1 * n1) // n2 * -1
+  else:
+    return n1 // n2
+
+
+def get_max_val(num, op):
+  # print("get_val" , num , op)
+  if max(op) == 0:
+    return num[0]
+  val = []
+  idx = get_idx(op)
+  for i in idx:
+    num2 = [cal(num[0], num[1], i)]
+    if len(num) > 2:
+      num2.extend(num[2:])
+    op2 = op[:]
+    op2[i] = op2[i] - 1
+    val.append(get_max_val(num2, op2))
+  return (max(val))
+
+
+def get_min_val(num, op):
+  # print("get_val" , num , op)
+  if max(op) == 0:
+    return num[0]
+  val = []
+  idx = get_idx(op)
+  for i in idx:
+    num2 = [cal(num[0], num[1], i)]
+    if len(num) > 2:
+      num2.extend(num[2:])
+    op2 = op[:]
+    op2[i] = op2[i] - 1
+    val.append(get_min_val(num2, op2))
+  return (min(val))
+
+
+print(get_max_val(num, op))
+print(get_min_val(num, op))
