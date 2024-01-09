7a788dce5b05ed1b6e5241009a8c540e5aa2ffa7
windylung <jisoolee031@gmail.com>
2024-01-09 17:42:52 +0000

    #14889

--- main.py
+++ main.py
@@ -1,66 +1,68 @@
-#14888
+#14889
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 
+
+def get_max(arr):
+  max_val = 0
+  # arr 전체 탐색하며 max 값 리턴
+  for i in range(N):
+    for j in range(N):
+      max_val = max(max_val, arr[i][j])
+  return max_val
+
+
 N = int(input())
-num = list(map(int, input().split()))
-op = list(map(int, input().split()))
+arr = [list(map(int, input().split())) for _ in range(N)]
 
 
-def get_idx(op):
-  idx = []
-  for i in range(4):
-    if op[i]:
-      idx.append(i)
-  return idx
+def get_add(arr):
+  for i in range(N):
+    arr[i] = [num for num in arr[i] if num != 0]
+    j = 0
+    while j < len(arr[i]) - 1:
+      if arr[i][j] == arr[i][j + 1]:
+        arr[i][j] *= 2
+        del arr[i][j + 1]
+      j += 1
+
+    while len(arr[i]) < N:
+      arr[i].append(0)
+  return arr
 
 
-def cal(n1, n2, op):
-  if op == 0:
-    return n1 + n2
-  elif op == 1:
-    return n1 - n2
-  elif op == 2:
-    return n1 * n2
-  elif op == 3 and n1 < 0 and n2 > 0:
-    return (-1 * n1) // n2 * -1
-  else:
-    return n1 // n2
+def move(arr, d):
+  cp_arr = arr[:]
+  res = arr[:]
+  if d == 0:
+    res = get_add(cp_arr)
+  elif d == 1:
+    for i in range(N):
+      cp_arr[i] = cp_arr[i][::-1]
+    res = get_add(cp_arr)
+    for i in range(N):
+      res[i] = res[i][::-1]
+  elif d == 2:
+    arr2 = list(zip(*cp_arr))
+    res = list(zip(*get_add(arr2)))
+
+  elif d == 3:
+    arr2 = list(zip(*cp_arr[::-1]))
+    res = list(zip(*get_add(arr2)))[::-1]
+  return res
 
 
-def get_max_val(num, op):
-  # print("get_val" , num , op)
-  if max(op) == 0:
-    return num[0]
-  val = []
-  idx = get_idx(op)
-  for i in idx:
-    num2 = [cal(num[0], num[1], i)]
-    if len(num) > 2:
-      num2.extend(num[2:])
-    op2 = op[:]
-    op2[i] = op2[i] - 1
-    val.append(get_max_val(num2, op2))
-  return (max(val))
+def get_val(arr, i):
+  if (i == 5):
+    return get_max(arr)
+  res = []
+  for d in range(4):
+    arr2 = move(arr, d)
+    # if i == 0  and d == 3:
+    # print("i:", i , "d: ", d, arr2, "res", res)
+    res.append(get_val(arr2, i + 1))
+  return max(res)
 
 
-def get_min_val(num, op):
-  # print("get_val" , num , op)
-  if max(op) == 0:
-    return num[0]
-  val = []
-  idx = get_idx(op)
-  for i in idx:
-    num2 = [cal(num[0], num[1], i)]
-    if len(num) > 2:
-      num2.extend(num[2:])
-    op2 = op[:]
-    op2[i] = op2[i] - 1
-    val.append(get_min_val(num2, op2))
-  return (min(val))
-
-
-print(get_max_val(num, op))
-print(get_min_val(num, op))
+print(get_val(arr, 0))
