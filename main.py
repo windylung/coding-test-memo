5ce650fa9d3838f3269f14f896744f44fdc23d53
windylung <jisoolee031@gmail.com>
2024-05-05 22:59:01 +0900

    #1504

--- main.py
+++ main.py
@@ -1,38 +1,54 @@
+#1504
 import sys
+import heapq
 
 input = sys.stdin.readline
-A = int(input())
-T = int(input())
-X = int(input())
+V, E = map(int, input().split())
+q = []
+# 노드 별 간선, 가중치 리스트
+data = list([] for _ in range(V + 1))
+# 각 노드까지 거리 리스트
 
+for i in range(E):
+    u, v, w = map(int, input().split())
+    data[u].append((w, v))
+    data[v].append((w, u))
+V1, V2 = map(int, input().split())
 
+def dij(start, end):
+    dist = list(float('inf') for _ in range(V + 1))
+    if start == end :
+        return 0
+    heapq.heappush(q, (0, start))
+    while q:
+        cur_dist, cur = heapq.heappop(q)
+        if cur_dist > dist[cur]:
+            continue
 
-# T의 값을 보고 -> 중간에 끊기지 않고 온전히 도는 cycle의 개수 확인
-cycle = 0 # 사이클의 개수
-cnt = 0 # 번 또는 데기의 갯수
-while (cnt < T) :
-    cycle += 1
-    cnt += cycle + 3
+        dist[cur] = cur_dist
+        for l_dist, l_node in data[cur]:
+            cmp = cur_dist + l_dist
+            if dist[l_node] < 0 or cmp < dist[l_node]:
+                dist[l_node] = cmp
+                heapq.heappush(q, (cmp, l_node))
+    return dist[end]
 
-# 0 1 0 1 0 0 1 1
+cal_list_1 = [(1, V1), (V1, V2), (V2, V)]
+cal_list_2 = [(1, V2), (V2, V1), (V1, V)]
 
-cnt -= cycle + 3
-cycle -= 1
+def cal(cal_list):
+    res = 0
+    for c1, c2 in cal_list:
+        c = dij(c1, c2)
+        if c == float('inf'):
+            return -1
+        res += c
 
-idx = 0
-diff = T-cnt # 사이클 내 번 또는 데기의 개수
-# print(cnt, cycle, diff)
+    return res
+res1 = cal(cal_list_1)
+res2 = cal(cal_list_2)
+if (res1 == -1 and res2 == -1):
+    print(-1)
+else :
+    print(min(res1, res2))
 
-if (diff == 1):
-    idx = X + 1
-elif (diff == 2):
-    idx = X + 3
-elif X == 0:
-    idx = 2 + diff  # 앞에 기본적으로 1010 있음
-else:
-    idx = 2 + (cycle+2) + diff
-
-res = cycle ** 2 + 7 * cycle + idx
-
-# print(res)
-print((res-1)%A)
